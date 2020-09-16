7cec0cc1179ee24565b9bd65582bd89f62556539
Santiago <ssantiagotl@gmail.com>
2020-09-16 09:32:42 -0300

    Initial commit - backend

---
+++ backend/digitalhouse-pi/src/main/java/com/pi/dh/DigitalhousePiApplication.java
@@ -0,0 +1,13 @@
+package com.pi.dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalhousePiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalhousePiApplication.class, args);
+	}
+
+}
---
+++ backend/digitalhouse-pi/src/test/java/com/pi/dh/DigitalhousePiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.dh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalhousePiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
