1e195233a62d6379dc969e51f8a4f61ac3fe5e05
Santiago <ssantiagotl@gmail.com>
2020-10-05 09:59:34 -0300

    CORS autorizado para todos os controllers

--- backend/digitalhouse-pi/src/main/java/com/pi/dh/api/controller/ProfessorController.java
+++ backend/digitalhouse-pi/src/main/java/com/pi/dh/api/controller/ProfessorController.java
@@ -3,6 +3,7 @@ package com.pi.dh.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.pi.dh.domain.service.ProfessorService;
 
 @RestController
 @RequestMapping("/professores")
+@CrossOrigin
 public class ProfessorController {
 
 	@Autowired
