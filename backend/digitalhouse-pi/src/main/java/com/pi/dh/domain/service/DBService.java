387d666b4fc99164528633267d637b99ae43744b
Santiago <ssantiagotl@gmail.com>
2020-10-06 12:36:15 -0300

    Adicionando arquivos de configuraçao para teste e desenvolvimento. Servico do banco criado (falta implementar)

---
+++ backend/digitalhouse-pi/src/main/java/com/pi/dh/domain/service/DBService.java
@@ -0,0 +1,15 @@
+package com.pi.dh.domain.service;
+
+import java.text.ParseException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+	
+	public void instantiateTestDatabase() throws ParseException {
+
+
+	}
+}
